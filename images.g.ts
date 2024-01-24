59f4be877a2ee27f57e354b60b46847812340068
MurderDieReal <153736096+MurderDieReal@users.noreply.github.com>
2024-01-24 12:27:49 +0000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f 7 7 7 7 f f f f f f 3 3 3 3 f 
+f 7 7 7 7 f f f f f f 3 3 3 3 f 
+f f f f f f 1 1 1 1 f f f f f f 
+f f f f f 1 1 f f 1 1 f f f f f 
+f f f f f f f f f 1 1 f f f f f 
+f f f f f f f f 1 1 1 f f f f f 
+f f f f f f f 1 1 f f f f f f f 
+f f f f f f f 1 1 f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f 1 1 f f f f f f f 
+f 5 5 5 5 f f 1 1 f f 4 4 4 4 f 
+f 5 5 5 5 f f f f f f 4 4 4 4 f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
